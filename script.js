cf7dd6401803fa600231ec4ea50241d2ca7a0c4e
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-12-08 13:49:04 +0100

    Modif

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
 let interf = document.getElementById("text");
-let buttons = document.getElementsByTagName("button");
+let buttons = document.getElementsByClassName("all");
 
 for(let button of buttons) {
-    button.addEventListener("click", function (){
-        switch(button.innerHTML) {
-            case "=":
-                text.innerHTML = eval(text.innerHTML);
-                break;
-            case "Reset":
-                text.innerHTML = "";
-                break;
-            case "Sup":
-                interf.innerText = interf.innerHTML.slice(0, interf.innerHTML.length - 1);
-                break;
-            case "Copier":
-                let range = document.createRange();
-                range.selectNode(interf);
-                window.getSelection().addRange(range);
-                document.execCommand("copy");
-                window.getSelection().removeAllRanges();
-                break;
-            default:
-                text.innerHTML += button.innerHTML;
-        }
+    button.addEventListener("click", function () {
+            text.innerHTML = (button.innerHTML === "=") ? eval(text.innerHTML) : (button.innerHTML === "Reset") ? "" : (button.innerHTML === "Sup") ? interf.innerHTML.slice(0, interf.innerHTML.length - 1) : text.innerHTML+ button.innerHTML;
     });
 }
+
+document.getElementById("copier").addEventListener("click", function () {
+    let range = document.createRange();
+    range.selectNode(interf);
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+})
+
+
